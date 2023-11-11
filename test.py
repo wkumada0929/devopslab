af4108b674cd937ecc572e2bd4d3d13b278facd8
wkumada0929 <149245641+wkumada0929@users.noreply.github.com>
2023-11-11 07:10:10 -0300

    Arrumando a Falha

--- test.py
+++ test.py
@@ -16,4 +16,4 @@ class Test(unittest.TestCase):
 
     def test_conteudo(self):
         # verifica o retorno do conteudo da pagina
-        self.assertEqual(self.result.data.decode('utf-8'), "Desafio Custom Message")
+        self.assertEqual(self.result.data.decode('utf-8'), "Fix Unit Test")
